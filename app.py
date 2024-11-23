cc3d93078086a66dc123bdf1de08a61e094589c9
Matheus Martins <103073881+Matsey-Matheus@users.noreply.github.com>
2024-11-23 02:54:46 +0000

    teste

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def previsao(interpreter,image):
     input_details = interpreter.get_input_details()[0]
     output_details = interpreter.get_output_details()
     
-    interpreter.set_tensor(input_details[0]['index'],image) 
+    interpreter.set_tensor(input_details['index'],image) 
     
     interpreter.invoke()
     
